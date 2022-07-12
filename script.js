381b53fdc4bca90cb1ae6591df4659f88ba2c215
Remison Junio <remisonpires@gmail.com>
2022-07-12 13:27:03 -0300

    Últimos ajustes

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 const estrutura = document.querySelector('#estrutura');
 const estruturaAbrirFechar = ()=> estrutura.classList.toggle('menuAberto');
+const estruturaFechar = ()=> estrutura.classList.remove('menuAberto');
 
 const btnMenu = document.querySelector('.btn-menu').addEventListener('click', estruturaAbrirFechar);
 const btnMenuFechar = document.querySelector('.btn-menu-fechar').addEventListener('click', estruturaAbrirFechar);
@@ -10,7 +11,59 @@ const btnMenuFechar = document.querySelector('.btn-menu-fechar').addEventListene
 
 const pesquisar = document.querySelector('.pesquisar');
 const pesquisaAbrirFechar = ()=> pesquisar.classList.toggle('active');
+const pesquisaFechar = ()=> pesquisar.classList.remove('active');
 
-const btnPesquisarAbrir = document.querySelector('.btn-pesquisar-abrir').addEventListener('click', pesquisaAbrirFechar);
+const search = document.querySelector('#search');
 
-const btnPesquisarFechar = document.querySelector('.btn-pesquisar-fechar').addEventListener('click', pesquisaAbrirFechar);
+const btnPesquisarBuscar = document.querySelector('.btn-pesquisar-buscar')
+
+const btnPesquisarFechar = document.querySelector('.btn-pesquisar-fechar')
+
+function pesquisarItem() {
+	pesquisar.classList.remove('active')
+	btnPesquisarBuscar.classList.remove('active')
+	btnPesquisarFechar.classList.add('active')
+	search.value = ''
+}
+
+btnPesquisarBuscar.addEventListener('click', pesquisarItem);
+
+const btnPesquisarAbrir = document.querySelector('.btn-pesquisar-abrir')
+btnPesquisarAbrir.addEventListener('click', pesquisaAbrirFechar);
+
+const pesquisarBG = document.querySelector('.pesquisar-bg').addEventListener('click', pesquisaAbrirFechar)
+
+btnPesquisarFechar.addEventListener('click', pesquisaAbrirFechar);
+
+search.addEventListener('input', (e)=> {
+	if(e.target.value === '') {
+		btnPesquisarFechar.classList.add('active')
+		btnPesquisarBuscar.classList.remove('active')
+	} else {
+		btnPesquisarFechar.classList.remove('active')
+		btnPesquisarBuscar.classList.add('active')
+	}
+})
+
+document.addEventListener('keyup', e => {
+	if(e.keyCode === 27) {
+		pesquisaFechar()
+	}
+
+	return
+})
+
+search.addEventListener('keyup', e => {
+	if (e.keyCode === 13) {
+		pesquisarItem()
+	}
+
+	return
+})
+
+window.addEventListener("resize", ()=>  {
+    let tamanhoTela = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    if (tamanhoTela >= 1000) {
+        estruturaFechar()
+    }
+});
